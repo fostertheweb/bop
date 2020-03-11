76a980d4681188599311bd5826dbc4ef0eb96022
Brendan McDonald <brendan.mcdonald@eagledream.com>
2020-03-11 19:55:21 -0400

    request is formed properly, callback needs help

--- server/src/app.js
+++ server/src/app.js
@@ -9,11 +9,17 @@ app.register(require("fastify-cors"), {
   methods: ["GET", "POST", "PUT", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
-  maxAage: 300,
+  maxAge: 300,
 });
 app.register(require("fastify-sensible"));
 
+app.register(require("fastify-cookie"), {
+  secret: "almond-milk", // we'll change this
+  parseOptions: {}, // options for parsing cookies
+});
+
 // routes
 app.register(require("./routes/spotify"), { prefix: "/spotify" });
+app.register(require("./routes/login"), { prefix: "/login" });
 
 module.exports = app;
--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { useSpotify } from "./hooks/use-spotify";
 
@@ -8,21 +7,11 @@ function App() {
   React.useEffect(() => {
     search("john mayer");
   }, []);
+
+  const login = () => {};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <a href="http://localhost:4000/login">login</a>
     </div>
   );
 }
