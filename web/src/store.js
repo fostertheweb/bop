8f942211fe6f58f3a0fc943c252a6b3a7c9626b8
Jonathan Foster <jxf9099@rit.edu>
2020-06-01 14:51:12 -0400

    cleaning crew just came through

--- web/src/store.js
+++ web/src/store.js
@@ -1,32 +1,22 @@
 import { useTransactionObservation_UNSTABLE } from "recoil";
 
 export function PersistenceObserver() {
-	useTransactionObservation_UNSTABLE(
-		({ atomValues, atomInfo, modifiedAtoms }) => {
-			console.log({ atomValues });
-			console.log({ modifiedAtoms });
-			console.log({ atomInfo });
-
-			for (const modifiedAtomKey of modifiedAtoms) {
-				console.log({ modifiedAtomKey });
-				localStorage.setItem(
-					modifiedAtomKey,
-					JSON.stringify(atomValues.get(modifiedAtomKey)),
-				);
-			}
-		},
-	);
+	useTransactionObservation_UNSTABLE(({ atomValues, modifiedAtoms }) => {
+		for (const modifiedAtomKey of modifiedAtoms) {
+			localStorage.setItem(
+				modifiedAtomKey,
+				JSON.stringify(atomValues.get(modifiedAtomKey)),
+			);
+		}
+	});
 
 	return null;
 }
 
 export const initializeState = ({ set }) => {
-	const keys = Object.keys(localStorage).filter((k) =>
-		k.startsWith("crowdQ.storage."),
-	);
-	const storageEntries = keys.map((key) => localStorage.getItem(key));
-	storageEntries.forEach((entry, i) => {
-		const key = keys[i];
-		set({ key }, JSON.parse(entry));
-	});
+	Object.entries(localStorage)
+		.filter(([key]) => key.startsWith("crowdQ.storage."))
+		.forEach(([key, value]) => {
+			set({ key }, JSON.parse(value));
+		});
 };
