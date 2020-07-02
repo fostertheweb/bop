b18ca9fae5633acf723f5e6379dae224d0e7b4fe
Jonathan Foster <jxf9099@rit.edu>
2020-07-01 23:35:44 -0400

    track

--- web/src/Components/tracks.js
+++ web/src/Components/tracks.js
@@ -1,10 +1,30 @@
 import React from "react";
 import { useTable } from "react-table";
 
-export default function TrackList() {
+export function TrackList() {
 	return (
 		<ul>
 			<li>track</li>
 		</ul>
+	);
+}
+
+export function Track({ item }) {
+	return (
+		<>
+			<div className="p-2">
+				<img
+					src={item.album.images[2].url}
+					alt="album art"
+					className="shadow w-10 h-10"
+				/>
+			</div>
+			<div className="ml-1">
+				<div className="text-gray-400">{item.name}</div>
+				<div className="text-gray-500">
+					{item.artists.map((artist) => artist.name).join(", ")}
+				</div>
+			</div>
+		</>
 	);
 }
