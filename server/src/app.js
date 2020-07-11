bed1c9e958eda088877ee9e0d985382857550c00
Jonathan Foster <fostertheweb@gmail.com>
2020-07-11 14:24:48 -0400

    brokeu

--- server/src/app.js
+++ server/src/app.js
@@ -1,28 +1,29 @@
 const app = require("fastify")({ logger: true });
 const io = require("socket.io")(app.server);
+const Redis = require("ioredis");
 
 const { REDIS_HOST, REDIS_PORT } = process.env;
 
 io.on("connection", (socket) => {
-	socket.on("join", (data) => {
-		console.log(`${data.user} joining ${data.room}`);
-		socket.join(data.room);
-		// socket.broadcast.to(data.room).emit("joined", `${data.user}, joined.`);
-		// io.to(data.room).emit("joined", `${data.user}, joined.`);
-	});
+  socket.on("join", (data) => {
+    console.log(`${data.user} joining ${data.room}`);
+    socket.join(data.room);
+    // socket.broadcast.to(data.room).emit("joined", `${data.user}, joined.`);
+    // io.to(data.room).emit("joined", `${data.user}, joined.`);
+  });
 
-	socket.on("clap", (data) => {
-		console.log(data);
-		socket.broadcast.to(data.room).emit("clap", `${data.user}, clapped!`);
-	});
+  socket.on("clap", (data) => {
+    console.log(data);
+    socket.broadcast.to(data.room).emit("clap", `${data.user}, clapped!`);
+  });
 
-	socket.on("addToQueue", (data) => {
-		socket.broadcast.to(data.room).emit("addToQueue", data.payload);
-	});
+  socket.on("addToQueue", (data) => {
+    socket.broadcast.to(data.room).emit("addToQueue", data.payload);
+  });
 
-	socket.on("queueUpdated", (data) => {
-		socket.to(data.room).emit("queueUpdate", data.payload);
-	});
+  socket.on("queueUpdated", (data) => {
+    socket.to(data.room).emit("queueUpdate", data.payload);
+  });
 });
 
 // health check
@@ -30,21 +31,21 @@ app.get("/ping", () => "PONG");
 
 // installed plugins
 app.register(require("fastify-cors"), {
-	origin: true,
-	methods: ["GET", "POST", "PUT", "PATCH"],
-	allowedHeaders: ["Content-Type", "Authorization"],
-	credentials: true,
-	maxAge: 300,
+  origin: true,
+  methods: ["GET", "POST", "PUT", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  maxAge: 300,
 });
 app.register(require("fastify-sensible"));
 
 app.register(require("fastify-cookie"), {
-	secret: "almond-milk", // we'll change this
-	parseOptions: {}, // options for parsing cookies
+  secret: "almond-milk", // we'll change this
+  parseOptions: {}, // options for parsing cookies
 });
 app.register(require("fastify-redis"), {
-	host: REDIS_HOST,
-	port: REDIS_PORT,
+  host: REDIS_HOST,
+  port: REDIS_PORT,
 });
 
 // routes
---
+++ server/src/routes/queue.js
@@ -0,0 +1,9 @@
+module.exports = function (app, _options, next) {
+  app.get("/:room/add", async function (req, res) {});
+
+  app.get("/:room/request", async function (req, res) {});
+
+  app.get("/:room/add", async function (req, res) {});
+
+  next();
+};
