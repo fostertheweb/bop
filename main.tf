17f91a4d43f5ebb6c21e648c8218033d0911e998
Jonathan Foster <fostertheweb@gmail.com>
2020-07-15 01:18:23 -0400

    could be playing valorant

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ resource "aws_route53_record" "cert_validation_api" {
 
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn, aws_route53_record.cert_validation_api.fqdn]
 }
 
 module "web" {
