f1df4f5daff3fcd1d5f924675661de71c71583f2
Jonathan Foster <fostertheweb@gmail.com>
2020-07-18 02:35:02 -0400

    al

--- server/main.tf
+++ server/main.tf
@@ -169,6 +169,6 @@ resource "aws_route53_record" "api" {
   alias {
     evaluate_target_health = true
     name                   = aws_api_gateway_domain_name.api.regional_domain_name
-    zone_id                = aws_api_gateway_domain_name.api.region_zone_id
+    zone_id                = aws_api_gateway_domain_name.api.regional_zone_id
   }
 }
