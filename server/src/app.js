4658d1d413ecf4502ab18355ace8c449a84c4534
Jonathan Foster <fostertheweb@gmail.com>
2020-08-01 22:12:59 -0400

    i think i finally understand aws

--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,9 @@ const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env;
 // health check
 app.get("/ping", () => "PONG");
 
+// yo
+app.get("/", (_, reply) => reply.send("OK"));
+
 // installed plugins
 app.register(require("fastify-cors"), {
   origin: true,
@@ -26,48 +29,6 @@ app.register(require("fastify-redis"), {
   host: REDIS_HOST,
   port: REDIS_PORT,
   password: REDIS_PASSWORD,
-});
-app.register(require("fastify-websocket"), {
-  clientTracking: true,
-});
-
-app.route({
-  method: "GET",
-  url: "/",
-  handler(_request, reply) {
-    return reply.send("OK");
-  },
-  wsHandler(connection) {
-    // app.websocketServer.clients
-    connection.socket.on("message", (message) => {
-      const { action, room, data, username } = JSON.parse(message);
-
-      switch (action) {
-        case "JOIN":
-          console.log(`${username} joined ${room}`);
-          break;
-        case "ADD_TO_QUEUE":
-          connection.socket.send(
-            JSON.stringify({
-              action: "SONG_ADDED",
-              data,
-            }),
-          );
-          break;
-        default:
-          console.log({ action });
-          console.log({ data });
-          console.log({ username });
-          break;
-      }
-    });
-
-    return {
-      statusCode: 200,
-      headers: { "Content-Type": "application/json" },
-      body: "Connected",
-    };
-  },
 });
 
 // routes
