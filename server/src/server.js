e3398ce50bf1b71de26c2e2cbba1bfc9fa30871e
Jonathan Foster <fostertheweb@gmail.com>
2021-04-22 12:36:23 -0400

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -171,10 +171,10 @@ async function getSpotifyTrack(trackId) {
 
 async function getYouTubeVideo(name, artists) {
   // get youtube video url
-  let video = await YouTube.searchOne(`${name} ${artists[0].name} audio`);
+  let video = await YouTube.searchOne(`${name} "${artists[0].name}" audio`);
   console.log("YouTube Request");
   if (!video) {
-    video = await YouTube.searchOne(`${name} ${artists[0].name}`);
+    video = await YouTube.searchOne(`${name} "${artists[0].name}"`);
     console.log("YouTube Request 2");
   }
 
