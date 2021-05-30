81e70d1bc20c254ba1251f694e6ae88f21f49b33
Jonathan Foster <fostertheweb@gmail.com>
2021-05-30 00:41:40 -0400

    don't need a client

--- player/build.rs
+++ player/build.rs
@@ -1,15 +1,15 @@
 fn main() {
-  let iface_files = &["proto/player.proto"];
-  let dirs = &["."];
+    let iface_files = &["proto/player.proto"];
+    let dirs = &["."];
 
-  tonic_build::configure()
-      .build_server(true)
-      .build_client(true)
-      .compile(iface_files, dirs)
-      .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(false)
+        .compile(iface_files, dirs)
+        .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
 
-  // recompile protobufs only if any of the proto files changes
-  for file in iface_files {
-      println!("cargo:rerun-if-changed={}", file);
-  }
+    // recompile protobufs only if any of the proto files changes
+    for file in iface_files {
+        println!("cargo:rerun-if-changed={}", file);
+    }
 }
